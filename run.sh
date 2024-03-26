9bd30379c604cda1da44cf715d61d047e68f9db0
rrsamal2008 <rrsamal2008@gmail.com>
2024-03-26 12:59:20 +0530

    Initial commit

---
+++ run.sh
@@ -0,0 +1,4 @@
+pip install -r requirements.txt
+# Run the Streamlit app
+# python -m streamlit run app.py --server.port $PORT --server.address 0.0.0.0
+python -m streamlit run app.py --server.port 8000 --server.address 0.0.0.0
